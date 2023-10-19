e9a7bdf5b1802a19980789f4dcb61a99164c5cc8
Ehad <ehadberisha@gmail.com>
2023-10-19 10:57:36 -0400

    gridbackgroundimages

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -5,14 +5,14 @@ function AboutUs() {
     <div className="aboutusclass">
       <h1>About Us</h1>
       <p>
-        Welcome to Ted's Auto Dealership, your one-stop destination for
-        buying and selling vehicles online. We're here to make the process of
-        buying or selling your car simple, convenient, and reliable. Your Auto
-        Marketplace is a team of dedicated car enthusiasts who understand the
-        unique bond between an individual and their vehicle. We share your
-        passion for all things automotive, which is why we've created this
-        platform to empower you to make informed decisions when it comes to
-        buying or selling your cars.
+        Welcome to Ted's Auto Dealership, your one-stop destination for buying
+        and selling vehicles online. We're here to make the process of buying or
+        selling your car simple, convenient, and reliable. Ted's Auto Dealership
+        is a team of dedicated car enthusiasts who understand the unique bond
+        between an individual and their vehicle. We share your passion for all
+        things automotive, which is why we've created this platform to empower
+        you to make informed decisions when it comes to buying or selling your
+        cars.
       </p>
     </div>
   );
